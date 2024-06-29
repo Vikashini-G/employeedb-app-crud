d37cb15eed89f890d1697d87e676dff6710f19bb
Vikashini G <vikashini.gopinath@gmail.com>
2024-06-29 23:47:51 +0530

    before comp

--- src/App.js
+++ src/App.js
@@ -4,38 +4,39 @@ import {db} from './firebase';
 import { collection, getDoc, getDocs, addDoc, doc, deleteDoc, updateDoc} from "firebase/firestore";
 
 function App() {
-  const [newName, setNewName] = useState("")
-  const [newAge, setNewAge] = useState(0)
-  const [newSal, setNewsal] = useState(0)
-  const [newDept, setNewDept] = useState("")
-  const [id, setId] = useState("")
-
-  const [show, setShow] = useState(false)
-
+  const [newName, setNewName] = useState('');
+  const [newAge, setNewAge] = useState(0);
+  const [newDOB, setNewDOB] = useState('');
+  const [newSal, setNewSal] = useState(0);
+  const [newDept, setNewDept] = useState('');
+  const [id, setId] = useState('');
+  const [show, setShow] = useState(false);
   const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
+  const usersCollectionRef = collection(db, 'users');
 
   const createUser = async () => {
-    await addDoc(usersCollectionRef, {Name: newName, Age: newAge, Sal: newSal, Dept: newDept});
+    await addDoc(usersCollectionRef, { Name: newName, Age: newAge, DOB: newDOB, Sal: newSal, Dept: newDept });
   };
 
   const updateUser = async () => {
-    const updateData = doc(db,"users",id)
-    await updateDoc(updateData, {Name: newName, Age: newAge, Sal: newSal, Dept: newDept});
-    setShow(false)
-    setNewName("")
-    setNewAge(0)
-    setNewsal(0)
-    setNewDept("")
+    const updateData = doc(db, 'users', id);
+    await updateDoc(updateData, { Name: newName, Age: newAge, DOB: newDOB, Sal: newSal, Dept: newDept });
+    setShow(false);
+    setNewName('');
+    setNewAge(0);
+    setNewDOB('');
+    setNewSal(0);
+    setNewDept('');
   };
 
-  const editUser = async (id, name, age, sal, dept) => {
-    setNewName(name)
-    setNewAge(age)
-    setNewsal(sal)
-    setNewDept(dept)
-    setId(id)
-    setShow(true)
+  const editUser = async (id, name, age, dob, sal, dept) => {
+    setNewName(name);
+    setNewAge(age);
+    setNewDOB(dob);
+    setNewSal(sal);
+    setNewDept(dept);
+    setId(id);
+    setShow(true);
   };
 
   const deleteUser = async (id) => {
@@ -67,11 +68,17 @@ function App() {
           setNewAge(event.target.value);
         }} 
       />
+      <input
+        type="date"
+        value={newDOB}
+        placeholder="Date of Birth..."
+        onChange={(event) => setNewDOB(event.target.value)}
+      />
       <input type="number" 
         value={newSal}
         placeholder="Salary..."
         onChange={(event) => {
-          setNewsal(event.target.value);
+          setNewSal(event.target.value);
         }} 
       />
       <input 
